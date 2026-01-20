016d803f74b6b393a3200696f582e5a4fb7eca61
Omarit82 <roselliomar82@gmail.com>
2026-01-20 19:56:13 -0300

    Added readme

---
+++ Readme.md
@@ -0,0 +1,10 @@
+*** USER MICROSERVICE ***
+User authentication service
+
+###Architecture###
+Node.js
+Express
+MongoDB + Mongoose
+JWT -> for authentication
+Bcrypt -> password hash
+Dotenv -> enviroment varibles
--- src/model/user.model.js
+++ src/model/user.model.js
@@ -1,7 +1,19 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-
-});
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        default: "user"
+    }
+}, { timestamps: true });
 
 export const userModel = mongoose.model("users", userSchema);
