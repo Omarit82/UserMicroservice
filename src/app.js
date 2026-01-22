c27e622f9d842e01e91afd8fa0003d19c110bff1
Omarit82 <roselliomar82@gmail.com>
2026-01-22 10:45:44 -0300

    Add: Cookie parser middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import userRouter from './routes/auth.routes.js';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 
 const app = express();
 
 app.use(cors());
+app.use(cookieParser(`${process.env.COOKIE_SECRET}`));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/auth', userRouter);
